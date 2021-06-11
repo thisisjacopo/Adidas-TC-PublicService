35fa999c716cd270b82daa0b8c3f59a698f0acd9
Jacopo Rodighiero <jacoporodighiero@Jacopos-MacBook-Pro-2.local>
2021-06-12 01:40:56 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+
+@Module({
+  imports: [SubscriptionModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
